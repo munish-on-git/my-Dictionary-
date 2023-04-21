5580652a0c79d99105d4a378ed7a8af0b655ae9f
Munish Pathania <110280635+munish-on-git@users.noreply.github.com>
2023-04-21 16:54:55 +0000

    more changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const searchBtn= document.querySelector(".search-btn");
 const info =document.querySelector(".info");
 const newWords = document.querySelector(".new-words");
 const containner = document.querySelector(".containner");
+let definitionsArray = [];
 
 
 
@@ -16,29 +17,49 @@ function searchfn(){
         `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`
       )
       .then((response)=> response.json())
-      .then((json) => data(json));
+      .then((json) => {
+        json.forEach((item) => {
+          let word = item.word;
+          let define = "";
+          let speech = "";
+          let phoneticText = "";
+          let phoneticsText2 = null;
+          let soundUK = "";
+          let soundUS = "";
+          let example = "";
+          let synonyms = [];
+          let antonyms = [];
+      
+          // check if meanings array exists and has at least one element
+      if (item.meanings && item.meanings.length > 0) {
+        define = item.meanings[0].definitions.map((definition) => {
+          return `<p>${definition.definition}</p>`;
+        }).join("");
 
-      function data(response){
-        let word = response[0].word;
-        let define  = response[0].meanings[0].definitions[0].definition;
-        let speech = response[0].meanings[0].partOfSpeech;
-        let phoneticText = response[0].phonetic;
-        let phoneticsText = response[0].phonetics[0].text;
+        example = item.meanings[0].definitions.example.map((ex) => {
+          return `<li>${ex.example}</li>`;
+        }).join("");
 
-        let synonyms = response[0].meanings[0].synonyms;
-        let antonyms = response[0].meanings[0].antonyms;
-        let phoneticsText1 = null;
-        let soundUK = response[0].phonetics[0].audio;
 
-        let soundUS = null; // initialize to null
+        speech = item.meanings[0].partOfSpeech;
 
-        // check if the phonetics array has a second element
-        if (response[0].phonetics.length > 1) {
-          soundUS = response[0].phonetics[1].audio;
-          phoneticsText1 = response[0].phonetics[1].text;
+
+        // check if phonetics array exists and has at least one element
+        if (item.phonetic && item.phonetic.length > 0) {
+          phoneticText = item.phonetic;
+          soundUK = item.phonetic.audio;
+
+          // check if phonetics array has a second element
+          if (item.phonetics && item.phonetics.length > 0) {
+          item.phonetics.map((ph) =>{
+            phoneticsText2 = ph.text;
+            soundUS = ph.audio;
+          })
+            
+          }
+
         }
-    
-
+       
         
           // creating a div to store search data
           const sDiv = document.createElement('div');
@@ -58,12 +79,28 @@ function searchfn(){
           <span>${phoneticText}</span>
           <span>US</span>
           <span class="fa-solid fa-volume-high us-sound hide"></span>
-          <span>${phoneticsText}</span>
+          <span>${phoneticsText2 ? phoneticsText2: ''}</span>
           </div>
           <hr>
-          <p> definition: ${define}</p>
-          <span> synonyms: ${synonyms}</span>
-          <span class="antonyms"> antonyms: ${antonyms}</span>
+          <p> ${define}</p>
+          <div> 
+          <ul>
+            <li>${example}</li>
+            </ul>
+          </div>
+          <div class="sy-an">
+          <ul>
+          <p>synonyms:</p>
+          <li>${synonyms}</li>
+          
+          </ul>
+          <ul>
+          <p>antonyms :</p>
+          <li>${antonyms}</li>
+          
+          </ul>
+          </div>
+          
           </div>
           </article>
           `;
@@ -87,11 +124,12 @@ function searchfn(){
         audio1.play();
       });
       
+    }
+  }
+        )}
 
-
-      }
+      )}
     
-    };
 
 
 
